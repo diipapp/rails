fc9bdc5c33fab071cec4294ae9d8e2bb62571559
7h3414n <alan86alves@gmail.com>
2022-03-08 15:15:46 -0300

    arquivo de configuracao do heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], scope: 'userinfo.email, userinfo.profile', skip_jwt: true
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], info_fields: 'name,email'
+  # config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], info_fields: 'name,email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
